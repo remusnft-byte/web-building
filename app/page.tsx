0324298aeb96692a9bac57d57c78b891f89084fd
remusofmars <remusofmars@TheRevenant.local>
2025-12-02 13:23:18 -0500

    Fix layout overlap using flexbox

--- app/page.tsx
+++ app/page.tsx
@@ -24,21 +24,21 @@ export default function Home() {
     }, []);
 
     return (
-        <div className="relative min-h-screen w-full overflow-hidden bg-white">
+        <div className="relative min-h-screen w-full overflow-hidden bg-white flex flex-col items-center pt-10">
             {/* Top Right Blob */}
-            <div className="absolute top-[-10%] right-[-5%] h-[500px] w-[500px] rounded-full bg-gradient-to-br from-pink-200 to-purple-200 opacity-60 blur-3xl filter" />
+            <div className="absolute top-[-10%] right-[-5%] h-[500px] w-[500px] rounded-full bg-gradient-to-br from-pink-200 to-purple-200 opacity-60 blur-3xl filter pointer-events-none" />
 
             {/* Top Right Line */}
-            <div className="absolute top-[15%] right-[5%] h-1 w-48 rotate-[-15deg] transform bg-gradient-to-r from-purple-400 to-purple-600 opacity-80" />
+            <div className="absolute top-[15%] right-[5%] h-1 w-48 rotate-[-15deg] transform bg-gradient-to-r from-purple-400 to-purple-600 opacity-80 pointer-events-none" />
 
             {/* Bottom Left Blob */}
-            <div className="absolute bottom-[-10%] left-[-5%] h-[500px] w-[500px] rounded-full bg-gradient-to-tr from-pink-200 to-purple-200 opacity-60 blur-3xl filter" />
+            <div className="absolute bottom-[-10%] left-[-5%] h-[500px] w-[500px] rounded-full bg-gradient-to-tr from-pink-200 to-purple-200 opacity-60 blur-3xl filter pointer-events-none" />
 
             {/* Bottom Left Line */}
-            <div className="absolute bottom-[15%] left-[5%] h-1 w-48 rotate-[-15deg] transform bg-gradient-to-r from-purple-400 to-purple-600 opacity-80" />
+            <div className="absolute bottom-[15%] left-[5%] h-1 w-48 rotate-[-15deg] transform bg-gradient-to-r from-purple-400 to-purple-600 opacity-80 pointer-events-none" />
 
             {/* Logo */}
-            <div className="absolute top-10 left-1/2 -translate-x-1/2 transform z-10">
+            <div className="relative z-10 mb-8">
                 <Image
                     src="/aintlabs-logo.png"
                     alt="Aint Labs Logo"
@@ -50,7 +50,7 @@ export default function Home() {
             </div>
 
             {/* Center Image */}
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <div className="relative z-10">
                 <Image
                     src="/characters.jpg"
                     alt="Characters"
@@ -65,7 +65,7 @@ export default function Home() {
             {mounted && positions.map((pos, index) => (
                 <div
                     key={index}
-                    className="absolute text-2xl text-gray-800"
+                    className="absolute text-2xl text-gray-800 z-20 pointer-events-none"
                     style={{
                         top: pos.top,
                         left: pos.left,
